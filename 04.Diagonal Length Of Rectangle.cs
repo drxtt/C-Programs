49d3e90ba2d7a6b225f1f1d1d3fa4c9ac7c9bdbb
Tushar Tiwari <141133473+drxtt@users.noreply.github.com>
2024-08-23 14:00:25 +0530

    04.Diagonal Length Of Rectangle.cs

--- 04.Diagonal Length Of Rectangle.cs
+++ 04.Diagonal Length Of Rectangle.cs
@@ -7,14 +7,13 @@ namespace DiagonalLengthOfRectangle
         static void Main(string[] args)
         {
             Console.WriteLine("LENGTH OF THE DIAGONAL OF A RECTANGLE\n");
-            Console.WriteLine("Enter the Length of the Rectangle : ");
+            Console.Write("Enter the Length of the Rectangle : ");
             int l = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Breadth of the Rectangle : ");
+            Console.Write("Enter the Breadth of the Rectangle : ");
             int b = Convert.ToInt32(Console.ReadLine());
 
-            int d = (int) Math.Sqrt(Math.Pow(l,2) + Math.Pow(b,2));
+            decimal d = (decimal) Math.Sqrt(Math.Pow(l,2) + Math.Pow(b,2));
             Console.WriteLine("The Length of the Diagonal is : {0}", d);
             Console.ReadKey();
         }
     }
-}
