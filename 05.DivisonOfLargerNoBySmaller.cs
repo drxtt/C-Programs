6d42d4d11585347716d0f2769a0b29023bdb92bc
Tushar Tiwari <141133473+drxtt@users.noreply.github.com>
2024-08-23 14:13:01 +0530

    05.DivisonOfLargerNoBySmaller.cs

---
+++ 05.DivisonOfLargerNoBySmaller.cs
@@ -0,0 +1,32 @@
+/*Write a program to pass 2 integer numbers as parameters.
+If either of the two numbers is 0, display invalid
+entry and the program should
+end, if it is valid entry, divide the larger number with the smaller
+number and display the result*/
+using System;
+
+namespace DiagonalLengthOfRectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIVISON OF LARGER NUMBER BY SMALLER NUMBER\n");
+            Console.Write("Enter first number : ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            if (n1 == 0 || n2 ==0)
+            {
+                Console.WriteLine("Invalid entry! Either of the two numbers is zero !");
+            }
+            else
+            {
+                float n3 = (float) Math.Max(n1, n2) / Math.Min(n1, n2);
+                Console.WriteLine("The result of the divison is : {0}", n3);
+            }
+            Console.ReadKey();
+        }
+    }
+}
